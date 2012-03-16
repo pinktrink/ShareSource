46a10fa3dd2cff12283523076afcb42b83080455
Eric Kever <lolbummer@gmail.com>
2012-03-16 15:25:18 -0400

    Don't need g modifier.

--- sharesource.php
+++ sharesource.php
@@ -1,7 +1,7 @@
 <?php
 if($_SERVER['QUERY_STRING'] === 'source'){
 	$file = file_get_contents($_SERVER['SCRIPT_FILENAME']);
-	$file = preg_replace_callback('@/\*HIDE(?:=(.+?))?\*/.*?/\*HIDE\*/@g', function($match){
+	$file = preg_replace_callback('@/\*HIDE(?:=(.+?))?\*/.*?/\*HIDE\*/@', function($match){
 		return empty($match[1]) ? '' : "/*{$match[1]}*/";
 	}, $file);
 	highlight_string($file);
