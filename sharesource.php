d6457dec6e25ec9a06346360713c76ac639f325e
Eric Kever <lolbummer@gmail.com>
2012-03-16 14:48:00 -0400

    Fixed drunk mistakes.

--- sharesource.php
+++ sharesource.php
@@ -1,7 +1,9 @@
 <?php
 if($_SERVER['QUERY_STRING'] === 'source'){
-	$file = file_get_contents($pathtothisfile); //I know this isn't correct, but I'll figure it out later, I'm kinda drunk at the moment and really should sleep soon.
-	preg_replace('@/\*HIDE(?:=(.*?))?\*/.*?/\*HIDE\*/@', '$1', $file);  //Again with the regex, drunk, don't care, I'll figure it out later.
-	highlight_string($file);  //This is right. I'm 99% sure it's right damnit.
+	$file = file_get_contents($_SERVER['SCRIPT_FILENAME']);
+	preg_replace_callback('@/\*HIDE(?:=(.+?))?\*/.*?/\*HIDE\*/@g', function($match){
+		return empty($match[1]) ? '' : "/*{$match[1]}*/";
+	}, $file);
+	highlight_string($file);
 }
 ?>
